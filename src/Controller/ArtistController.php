4e4528d4ae5a4d7a02eab5dcc98eb8067a1f2e0c
Ariti <rincon_adriano@yahoo.com>
2019-12-03 16:21:24 +0100

    initial commit

---
+++ src/Controller/ArtistController.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/artist",name="artist_")
+ */
+class ArtistController extends AbstractController
+{
+    /**
+     * @Route("-list",name="list")
+     */
+    public function index()
+    {
+        return $this->render('artists/list.html.twig');
+    }
+}
